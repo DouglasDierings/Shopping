56d51f677f136d804de64c5ac796addc5b52a043
Douglas Vinicius Dierings <douglasbindi@hotmail.com>
2023-07-07 14:49:09 +0100

    Add comments to class ConnectionDAO

--- Shopping/src/databaseDAO/ConectionDAO.java
+++ Shopping/src/databaseDAO/ConectionDAO.java
@@ -6,19 +6,23 @@ import java.sql.SQLException;
 
 public class ConectionDAO {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/store";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/store"; // Database URL
+    private static final String DB_USER = "root"; // Database username
+    private static final String DB_PASSWORD = ""; // Database password
 
+    /**
+     * Establishes a connection to the database
+     * 
+     * @return the connection object
+     * @throws SQLException if a database access error occurs
+     */
     public Connection connectToDB() throws SQLException {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
         } catch (SQLException error) {
-            // Lida com a exceção adequadamente (pode relançar ou registrar o erro)
             throw error;
         }
         return conn;
     }
-
 }
